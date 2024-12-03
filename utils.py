b2b41347b758b5c7c486c3f8ba25d39ad698b5ef
sadhvijak <sadhvija.kotagiri@nexturn.com>
2024-12-03 10:27:00 +0530

    utils.py

--- utils.py
+++ utils.py
@@ -3,16 +3,12 @@ import os
 import openai
 from dotenv import load_dotenv
 import base64
-
 import streamlit as st
-load_dotenv()
-os.environ["OPENAI_API_KEY"] = "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"
-openai.api_key = os.environ["OPENAI_API_KEY"]
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-# os.environ["OPENAI_API_KEY"] = "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"
-# openai.api_key = os.environ["OPENAI_API_KEY"]
-# client = OpenAI(api_key=api_key)
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 def get_answer(messages):
     system_message = [{"role": "system", "content": "You are an helpful AI chatbot, that answers questions asked by User."}]
@@ -32,16 +28,16 @@ def speech_to_text(audio_data):
         )
     return transcript
 
-def text_to_speech(input_text):
-    response = client.audio.speech.create(
-        model="tts-1",
-        voice="nova",
-        input=input_text
-    )
-    webm_file_path = "temp_audio_play.mp3"
-    with open(webm_file_path, "wb") as f:
-        response.stream_to_file(webm_file_path)
-    return webm_file_path
+# def text_to_speech(input_text):
+#     response = client.audio.speech.create(
+#         model="tts-1",
+#         voice="nova",
+#         input=input_text
+#     )
+#     webm_file_path = "temp_audio_play.mp3"
+#     with open(webm_file_path, "wb") as f:
+#         response.stream_to_file(webm_file_path)
+#     return webm_file_path
 
 def autoplay_audio(file_path: str):
     with open(file_path, "rb") as f:
