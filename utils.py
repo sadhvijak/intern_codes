7abf106dd5e3e9059035f3f859099c0643741d16
sadhvijak <sadhvija.kotagiri@nexturn.com>
2025-01-06 14:09:00 +0530

    utils.py

--- utils.py
+++ utils.py
@@ -6,9 +6,29 @@ import base64
 import streamlit as st
 
 
-load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+from cryptography.fernet import Fernet
+import json
+
+# Load the encryption key
+with open('secret.key', 'rb') as key_file:
+    key = key_file.read()
+        
+    cipher_suite = Fernet(key)
+
+    # Load the encrypted configuration data
+    with open('config.json', 'r') as config_file:
+        encrypted_data = json.load(config_file)
+
+    # Decrypt the sensitive information
+    # decrypted_data = {key: cipher_suite.decrypt(value.encode()).decode() for key, value in encrypted_data.items()}
+    decrypted_data = {
+        k: cipher_suite.decrypt(v.encode()).decode() 
+        for k, v in encrypted_data.items()
+    }
+# print(data["API_KEY"])
+api_key = decrypted_data.get("API_KEY")
+print(f"🔑 Decrypted API Key: {api_key}")
+client = OpenAI(api_key=api_key)
 
 def get_answer(messages):
     system_message = [{"role": "system", "content": "You are an helpful AI chatbot, that answers questions asked by User."}]
